2b981250133dcc60ca71ac8a1c4290c24987831d
Mohammadrezakhalili08 <99599430+Mohammadrezakhalili08@users.noreply.github.com>
2022-06-02 18:22:19 +0430

    Add files via upload

---
+++ banner/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def anothepage (request):
+    return render(request,'banner/another_page.html',context={})
+def contact (request):
+    return render(request,'banner/contact.html',context={})
+def examples (request):
+    return render(request,'banner/examples.html',context={})
+def index (request):
+    return render(request,'banner/index.html',context={})
+def page (request):
+    return render(request,'banner/page.html',context={})
